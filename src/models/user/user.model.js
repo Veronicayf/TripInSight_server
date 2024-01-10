0db100d229cc76c7291af2b062cb73a14e1096fa
Veronica Araque Franco <veronica.araque16@gmail.com>
2024-01-10 11:25:47 -0300

    definition user model and add folder models

---
+++ src/models/user/user.model.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+    sequelize.define('user', {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: DataTypes.UUIDV4
+        },
+        forename: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        surname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        nationality: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        image: {
+            type: DataTypes.STRING, //tipo URL buscar
+            allowNull: true,
+        },
+        birthDate: {
+            type: DataTypes.STRING, //formato fecha dd/mm/yyyy
+            allowNull: false
+        },
+        //A CONVERSAR
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        phoneNumber: {
+            type: DataTypes.NUMBER,
+            allowNull: false,
+        }
+    }, { timestamps: false });
+}
