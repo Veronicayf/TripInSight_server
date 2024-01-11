4f7c674754637242ec2d7ec23f0ba054d9fb085a
RaulCont <124114339+RaulCont@users.noreply.github.com>
2024-01-11 13:27:02 -0600

    db connection established
    otro commit

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const server = require('./server/src/server')
 const { conn } = require('./server/src/sync/dbConnection');
-const { postUser } = require('./server/src/controllers/User/user.postController'); //! para probar postUser.
 require('dotenv').config();
 
 conn.sync({ force: true }).then(() => {  
@@ -9,20 +8,4 @@ conn.sync({ force: true }).then(() => {
         console.log(`Sevidor corriendo en puerto: ${ process.env.PORT }` );        
     });   
 
-});
-
-
-
-
-
-
-
-
-
-
-//! app.post('/', (req, res) => {        
-//     // name, lastname, email, birthdate, nationality, phone
-//     const ans = postUser('Raul', 'Contreras', 'ccontrerasvelasco@gmail.com', '30/11/1993', 'mexican', 555555);
-//     // console.log(ans);
-//     return res.json(ans);
-// })    
+}); 
