554b8dd8d1599d5929774e21cf219743787b384c
Veronica Araque Franco <veronica.araque16@gmail.com>
2024-01-24 09:01:25 -0300

    last configuraions

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const PORT = 4000
 
 conn.sync({ force: false }).then(() => {  //true=borra todo
 
-
-
     server.listen(PORT, () => {
         console.log(`Sevidor corriendo en puerto: ${PORT}`);
     });
 
-}); 
+});
+
