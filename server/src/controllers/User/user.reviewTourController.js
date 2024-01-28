bd63232462379b7cbf75bf2cfd3dcdb953097de6
Guido Contartese <79114533+guidounion6@users.noreply.github.com>
2024-01-28 12:59:31 -0300

    Update user.reviewTourController.js
    last comment on controller

--- server/src/controllers/User/user.reviewTourController.js
+++ server/src/controllers/User/user.reviewTourController.js
@@ -34,8 +34,10 @@ const reviewTour = async(idUser, idTour, review)=> {
           
         const newReview = review.create({
 
-            //faltan las props
-            
+            //falta saber si los comentarios van en un modelo nuevo o actualizan alguno anterior
+            // en caso de actualizar uno, habria que modificar la ruta y que sea un put en lugar de 
+            //un post 
+
         })
 
         return newReview
