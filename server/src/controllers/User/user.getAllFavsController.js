ed54f0368a9a9a608134f2a53f947877021f5531
Guido Contartese <79114533+guidounion6@users.noreply.github.com>
2024-01-28 10:58:43 -0300

    Get all favs route finished

--- server/src/controllers/User/user.getAllFavsController.js
+++ server/src/controllers/User/user.getAllFavsController.js
@@ -2,10 +2,39 @@ const { user, tour, favorites_tours } = require("../../sync/dbConnection");
 
 const getAllFavs = async(id)=> {
 
-    const allFavs = await favorites_tours.findByPk(id);
+    try {
+        
+        const allFavsUser = await user.findByPk(id);
+    
+        if (!allFavsUser) return ({
+            errors: {
+                page: {
+                    type: "query",
+                    value: "id",
+                    msg: `the user with the id ${id} does not exists`,
+                    path: "id",
+                    location: "query"
+                }
+            }
+        })
+    
+        const allFavs = await favorites_tours.findAll({
+          where: { id: id },
+          include: [{
+            model: tour,
+            attributes: ['id', 'nameTour', "initialDate", 'endDate', 'image', 'continent', 'country', 'city', 
+        'type', 'capacity', 'subscription', 'places', 'description', 'season', 'status', 'price', 'equipment', 
+    'tags', 'photos' ], 
+          }],
+        });
+    
+        return allFavs;
+      } catch (error) {
+      
+        throw error;
+      }
+    }
 
-    return 
-}
 
 module.exports = { getAllFavs }
 
