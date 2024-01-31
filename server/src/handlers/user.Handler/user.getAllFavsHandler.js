cf0f5a9209c9d2ec8cf385420aeba2bc23cb7063
Veronica Araque Franco <veronica.araque16@gmail.com>
2024-01-31 14:09:44 -0300

    se arreglo el envio de las notificaciones

--- server/src/handlers/user.Handler/user.getAllFavsHandler.js
+++ server/src/handlers/user.Handler/user.getAllFavsHandler.js
@@ -2,30 +2,30 @@ const { getAllFavs } = require("../../controllers/User/user.getAllFavsController
 //const { validationResult } = require('express-validator');
 
 
-const getAllFavsHandler = async (req,res) => {
+const getAllFavsHandler = async (req, res) => {
 
     const { id } = req.params
-      
-           try { 
 
-            const allFavs = await getAllFavs(id)
-             if ( allFavs.length === 0 ){
-             return res.status(404).json({
+    try {
+
+        const allFavs = await getAllFavs(id)
+        if (allFavs.length === 0) {
+            return res.status(404).json({
                 errors: {
                     msg: `there are no favs for the user ${allFavs.name} with the id ${id}`,
 
                 }
-            }); 
+            });
         }
-            
+
         if (allFavs.errors) {
             return res.status(400).json(allFavs);
         }
 
         return res.json(allFavs)
-           } catch (error) {
-            return res.status(400).json({ error: error });
-           }
+    } catch (error) {
+        return res.status(400).json({ error: error });
+    }
 }
 
 module.exports = { getAllFavsHandler }
