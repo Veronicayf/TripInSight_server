59e99037dfd860d5aaa1c889c07ee1368af9f11e
guido_union6 <guido_union6@hotmail.com>
2024-01-31 22:44:04 -0300

    Update tours.getHandler.js
    last change

--- server/src/handlers/tours.Handler/tours.getHandler.js
+++ server/src/handlers/tours.Handler/tours.getHandler.js
@@ -2,7 +2,7 @@
 const { getAllTours } = require('../../controllers/tours/tours.getController')
 
 const getAllToursHandler = async (req, res) => {
-    const { page = 1, pageSize = 1000 } = req.query
+    const { page = 1, pageSize = 100 } = req.query
     try {
         const allTours = await getAllTours(page, pageSize);
         res.status(200).json(allTours)
