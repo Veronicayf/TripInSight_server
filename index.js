b2a63a8024f34ea0574205d5c44bc01a8f34ea74
guido_union6 <guido_union6@hotmail.com>
2024-01-31 21:05:25 -0300

    update server

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 const server = require('./server/src/server')
 const { conn } = require('./server/src/sync/dbConnection');
 require('dotenv').config();
-const PORT = 4000
+
 
 
 conn.sync({ force: true }).then(() => {  //true=borra todo
 
 
-    server.listen(PORT, () => {
-        console.log(`Sevidor corriendo en puerto: ${PORT}`);
+    server.listen(process.env.DBPORT, () => {
+        console.log(`Sevidor corriendo en puerto: ${process.env.DBPORT}`);
     });
 
 });
